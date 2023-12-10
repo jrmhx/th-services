ee7b34d7cb1311c98a30ad32321e02c1a2590690
u7439274 <u7439274@anu.edu.au>
2023-12-10 15:45:30 +1100

    add spring gateway, open USER service to external client

---
+++ apigw/src/main/java/com/treasurehunt/apigw/ApiGWApplication.java
@@ -0,0 +1,11 @@
+package com.treasurehunt.apigw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiGWApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ApiGWApplication.class, args);
+    }
+}
